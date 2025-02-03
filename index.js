9a5e9928c09854a4e3f2ccfd106c7ab22c08e2a5
Giv3Grac3 <yayagrace31@gmail.com>
2025-02-03 10:52:44 -0500

    update

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ const myBox = document.getElementById("myBox");
 console.log(myBox);
 
 function changeColor(event) {
-  // I do not see pointerEvent under inspect, console
-  // console.log(event);
-  event.target.style.backgroundColor = "tomato";
-  event.targettextContent = "OUCH! 🤕"
+  console.log(event);
+  //   event.target.style.backgroundColor = "tomato";
+  //   event.targettextContent = "OUCH! 🤕"
 }
 
 myBox.addEventListener("click", changeColor);
